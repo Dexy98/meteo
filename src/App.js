fa2cb736638fb1a4805d203f7dda5b3fe38b7d15
Dexy98 <davidecandia@live.it>
2023-07-14 21:48:40 +0200

    fine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,59 @@
+import axios from 'axios';
 import './App.css';
-
+import { useState } from 'react'
+import { BsSearch } from 'react-icons/bs'
 function App() {
+  const [city, setCity] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [weatherData, setWeatherData] = useState(null);
+
+
+  const apiKey = process.env.REACT_APP_WEATHER_KEY;
+
+  const fetchWeather = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+      );
+      setWeatherData(response.data);
+      console.log(response.data)
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+
+  };
   return (
-    <div className="App">
-      ciao
+    <div className='bg-gray-600 mx-auto flex justify-center max-w-xl flex-col'>
+      <div className='bg-gray-600 mx-auto flex justify-center max-w-xl p-3
+    '>
+        <form onSubmit={fetchWeather} className='flex items-center'>
+          <input
+            className='bg-gray-600 text-white p-2 border border-black'
+            type="text"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          />
+          <button type="submit">
+            <BsSearch size={30} className='text-white mx-2' />
+          </button>
+        </form>
+        {loading && <div>Caricamento...</div>}
+        {loading && <div>Caricamento...</div>}
+
+      </div>
+      {weatherData && (
+        <div className='flex items-center p-4 h-screen text-white flex-col'>
+          <h2 className='text-6xl'>{weatherData.name}</h2>
+          <p className='text-1xl'>{weatherData.weather.description}</p>
+          <p className='text-6xl '>{weatherData.main.temp}°C</p>
+        </div>
+      )}
     </div>
+
   );
 }
 
