fd07930e4397b24d72e4b770524baf0ccc74b8b3
Dexy98 <davidecandia@live.it>
2023-08-22 16:21:14 +0200

    agg.

--- src/App.js
+++ src/App.js
@@ -42,6 +42,16 @@ function App() {
       setWeatherData(response.data);
       console.log(response.data)
 
+      let ora = response.data.list[0].dt_txt
+      const currentDate = new Date();
+      const tomorrowDate = currentDate.getDate()+ 1 +"-" +( currentDate.getMonth()+1) ;
+
+      
+      const parts = ora.split(' ')
+      const data = parts[0]
+      const time = parts[1]
+
+
 
       setLoading(false);
     } catch (error) {
@@ -54,16 +64,17 @@ function App() {
   const speedInKmh = weatherData?.list[0].wind.speed * 3.6;
   const roundedSpeed = Math.round(speedInKmh);
   return (
-    <div className='bg-gray-600 mx-auto flex justify-center max-w-xl flex-col h-full transition-all drop-shadow-2xl shadow-black shadow-xl'>
+    <div className=' bg-[#0B0E12] mx-auto flex justify-center max-w-xl flex-col h-full transition-all drop-shadow-2xl shadow-black shadow-xl rounded-b-3xl'>
+          
       {loading && <div><Lottie animationData={Loading} loop={true}/></div>}
 
       <h1 className='flex justify-center text-white text-4xl font-bold -tracking-tighter mt-1'>METEO</h1>
-      <div className='bg-gray-600 mx-auto flex justify-center max-w-xl p-3
+      <div className='bg-[#0B0E12] mx-auto flex justify-center max-w-xl p-3
     '>
 
         <form onSubmit={fetchWeather} className='flex items-center '>
           <input
-            className='bg-gray-600 text-white p-2 border border-black shadow-sm shadow-black'
+            className='bg-[#0B0E12] text-white p-2 border border-black shadow-sm shadow-black'
             type="text"
             value={city}
             onChange={(e) => setCity(e.target.value)}
@@ -76,7 +87,7 @@ function App() {
       </div>
       {weatherData && (
         <div className='flex items-center p-4 h-full text-white flex-col transition-all'>
-          <h2 className='pt-3 text-6xl text-red-500 drop-shadow-lg shadow-neutral-800 uppercase tracking-tighter font-black'>{weatherData.city.name}</h2>
+          <h2 className='pt-3 text-4xl text-[#9FE6FD] drop-shadow-lg shadow-neutral-800 uppercase tracking-tighter font-black'>{weatherData.city.name}</h2>
           <p className=' pt-3 text-3xl text capitalize drop-shadow-md shadow-black font-normal'>{weatherData.list[0].weather[0].description}</p>
           <img
             src={getWeatherImage(weatherData.list[0].weather[0].main)}
@@ -87,7 +98,6 @@ function App() {
           <p className=' pt-3 text-2xl text capitalize drop-shadow-md shadow-black font-thin'>Visibilità: {visibilityInKM} Km</p>
           <p className=' text-2xl text capitalize drop-shadow-md shadow-black font-thin'>Umidità: {weatherData.list[0].main.humidity} %</p>
           <p className=' text-2xl text  drop-shadow-md shadow-black font-thin'>Vento: {roundedSpeed} Km/h</p>
-
           <article className='w-full overflow-x-auto mt-8'>
             <div className=' grid-flow-col gap-5 inline-grid' style={{gridAutoColumns: "15ch"}}  >
             <WeatherCard weatherData={weatherData} getWeatherImage={getWeatherImage} index={1} /> 
@@ -107,5 +117,4 @@ function App() {
 
   );
 }
-
 export default App;
